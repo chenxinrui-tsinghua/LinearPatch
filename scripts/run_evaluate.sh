c068784319d6cb14688538411345a8c209a2538f
chenxinrui-tsinghua <cxr22@tsinghua.org.cn>
2025-12-05 11:58:52 +0800

    Update run_evaluate.sh

--- scripts/run_evaluate.sh
+++ scripts/run_evaluate.sh
@@ -3,6 +3,7 @@
 # --model /path-to/llama-2-7b \
 # --max_memory "24GiB" \
 # --eval_ppl \
+# --eval_mmlu \
 # --eval_tasks "wsc273,hellaswag,piqa,arc_easy,arc_challenge,boolq,winogrande,race,copa"
 
 ########################## Template ############################
